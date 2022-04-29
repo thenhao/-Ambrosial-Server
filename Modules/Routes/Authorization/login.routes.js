c643054ab8cf1d14e5b46d4cbd5359d24ab5ce31
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-04-29 11:05:48 +0800

    renamed files

--- Modules/Routes/Authorization/login.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const LoginController = require('../../Controller/Authorization/loginController');
-const loginController = new LoginController();
-
-router.post('/login', loginController.login);
-
-module.exports = router;
