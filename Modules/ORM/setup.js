130eed861899377c6d51f279ad06b92899aa4ce3
thenhao <94664145+thenhao@users.noreply.github.com>
2022-05-01 17:12:17 +0800

    added new sequelize heroku database

--- Modules/ORM/setup.js
+++ Modules/ORM/setup.js
@@ -1,7 +1,8 @@
 const {Sequelize} = require('sequelize');
 //for Regina, created for prisma
 //postgres://bjdhdvfkorbxgj:f21ee80e3cc9e58ae8febb7cf9c91961144d014892e5a475ea9c2f00f20128ea@ec2-3-223-213-207.compute-1.amazonaws.com:5432/da0394ou3o9iv1
-const sequelize = new Sequelize('postgres://cyckzdbkuiefxw:2212d06490232346bc459e61e26a56bde4dac52e25e20feacc3b2e01642eb1d7@ec2-3-211-6-217.compute-1.amazonaws.com:5432/d38t9blqhpu6i1', {
+
+const sequelize = new Sequelize('postgres://ewjsdoixvpdifh:ddb937710577b50385a013604346e38da96036269ee49c51195f04c546ba96a0@ec2-3-218-171-44.compute-1.amazonaws.com:5432/daa90j9j7cfvbb', {
   dialectOptions: {
     ssl: {
       require: true,
