408a3044a5ac72a06570c941da886d84227ac0fa
thenhao <94664145+thenhao@users.noreply.github.com>
2022-05-07 00:07:13 +0800

    added cors option

--- Modules/Routes/index.js
+++ Modules/Routes/index.js
@@ -4,7 +4,13 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
 
 //Authorization
 const register = require('../Routes/Authorization/register.route');
