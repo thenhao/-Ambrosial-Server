a6f49fed439234effd16f9a4245b463f7b672d92
thenhao <94664145+thenhao@users.noreply.github.com>
2022-05-10 00:02:51 +0800

    edited the cors origin

--- Modules/Routes/index.js
+++ Modules/Routes/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    //origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
